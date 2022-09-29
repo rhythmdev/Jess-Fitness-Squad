70ade45452435832116dfc5f3f8a2e0dd8bf46c7
rhythmdev <rhythmbiswas@gmail.com>
2022-09-29 10:26:03 +0600

    fixing

--- jes-fintess-squad/src/App.js
+++ jes-fintess-squad/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="">
-     
+    <div className="App">
+     <Header></Header>
     </div>
   );
 }
