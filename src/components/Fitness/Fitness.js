421b1b8704b77974fb0e17836941d793753ef957
rhythmdev <rhythmbiswas@gmail.com>
2022-09-29 14:25:13 +0600

    card added

--- src/components/Fitness/Fitness.js
+++ src/components/Fitness/Fitness.js
@@ -1,19 +1,24 @@
-import React from 'react';
-// import './Fitness.css'
+import React from "react";
+import "./Fitness.css";
 
 const Fitness = (props) => {
-  const {name, img, details, age, time} = props.info;
-    return (
-        <div className='info'>
-            <div className='card h-100'>
-            <img src={img} className='card-img-top img-thumbnail'  alt=''/>
-            <div className="card-body">
-                <h4>{name}</h4>
-            </div>
-            </div>
-       
+  const { name, img, details, age, time } = props.info;
+  return (
+    <div className="info">
+      <div className="card h-100 border-none shadow-sm">
+        <img src={img} className="card-img-top img-thumbnail" alt="" />
+        <div className="card-body">
+          <h4>{name}</h4>
+          <p>{details}</p>
+          <p className="fw-semibold">For Age: {age}</p>
+          <p className="fw-semibold">Time required: {time}m</p>
         </div>
-    );
+        <div className="card-footer">
+          <button className="add-cart-btn">Add to list</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Fitness;
--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,36 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import Fitness from '../Fitness/Fitness';
+import React, { useEffect, useState } from "react";
+import Fitness from "../Fitness/Fitness";
 
 const Information = () => {
-    const [informations, setInformations] = useState([]);
+  const [informations, setInformations] = useState([]);
 
-    useEffect( ()=>{
-        fetch('informations.json')
-        .then(res=> res.json())
-        .then(data=> setInformations(data))
-    }, [])
-    return (
-        <div className='row'>
-            <div className="col-lg-8">
-            <h4>Select Today's Fitness Track</h4>
-            <div className="card-area row row-cols-1 row-cols-md-3 g-4">
-                
-                         
-                 
-                   
-                {
-                   informations.map(info=> <Fitness key ={info.id} info ={info}></Fitness>)
-                }
-               </div>
-            </div>
-            <div className="col-lg-4">
-            <div className="info-area ">
-             <h2>Info area</h2>
-            </div>
-            </div>
-            
+  useEffect(() => {
+    fetch("informations.json")
+      .then((res) => res.json())
+      .then((data) => setInformations(data));
+  }, []);
+  return (
+    <div className="row">
+      <div className="col-lg-8">
+        <h4>Select Today's Fitness Track</h4>
+        <div className="container mx-3 mt-5">
+          <div className="card-area row row-cols-1 row-cols-md-3 g-4">
+            {informations.map((info) => (
+              <Fitness key={info.id} info={info}></Fitness>
+            ))}
+          </div>
         </div>
-    );
+      </div>
+      <div className="col-lg-4">
+        <div className="info-area ">
+          <h2>Info area</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Information;
